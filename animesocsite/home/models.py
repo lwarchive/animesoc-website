aed7e80a81ea31bf6f52664117edcf5f50781a5b
Stephanie Flower <stephanie.flower.15@gmail.com>
2021-08-18 21:17:32 +0100

    fixed the event model
    you can now see and edit it on the admin page

--- animesocsite/home/models.py
+++ animesocsite/home/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    event_name = models.CharField(max_length=200)
+    name = models.CharField("Event name", max_length=200, default="Untitled_Event")
     event_time = models.DateTimeField('Time')
     show_event = models.BooleanField(default=False)
     event_location = models.CharField(max_length=200)
     event_description = models.CharField(max_length=1000)
-    
+
+    def __str__(self):
+        return self.event_time.strftime("%d") + "/" + self.event_time.strftime("%m") + ": " + self.name
