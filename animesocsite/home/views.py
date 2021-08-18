669d126f6ea8f2f10c3b63ce58e53326ba9b1c39
Stephanie Flower <stephanie.flower.15@gmail.com>
2021-08-18 21:27:19 +0100

    the events now display on the page
    theres title, time, location and description

--- animesocsite/home/views.py
+++ animesocsite/home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 
 
 def index(request):
-    return render(request, 'home.html')
+
+    events = Event.objects.all()
+
+    context = {
+        'events' : events
+    }
+
+    return render(request, 'home.html', context)
