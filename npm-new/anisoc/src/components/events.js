c4a6b69f6144860ba343486f620e941386fff4a2
Leo Durrant <poweredbykurisu@hotmail.com>
2022-07-25 17:41:00 +0100

    feat: added events section

--- npm-new/anisoc/src/components/events.js
+++ npm-new/anisoc/src/components/events.js
@@ -1,56 +1,79 @@
-import '../index.css';
-import Event1 from '../static/events/event1.png';
-import Event2 from '../static/events/event2.png';
-import Event3 from '../static/events/event3.png';
+import "../index.css";
+import Event1 from "../static/events/event1.png";
+import Event2 from "../static/events/event2.png";
+import Event3 from "../static/events/event3.png";
+
+const placeholderText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum egestas neque at justo tincidunt egestas. Integer mauris diam, fringilla eget gravida condimentum, lacinia ut justo. Proin cursus dolor a laoreet luctus. Vestibulum vitae feugiat quam, condimentum dictum eros. Morbi eleifend lorem sit amet bibendum dignissim. ";
 
 const events = [
-    {"Title" : "Event 1",
-    "Date" : "01/01/2020",
-    "Time" : "12:00",
-    "Location" : "Location 1",
-    "Description" : "Description 1",
-    "Image" : Event1},
-    {"Title" : "Event 2",
-    "Date" : "01/01/2020",
-    "Time" : "12:00",
-    "Location" : "Location 2",
-    "Description" : "Description 2",
-    "Image" : Event2},
-    {"Title" : "Event 3",
-    "Date" : "01/01/2020",
-    "Time" : "12:00",
-    "Location" : "Location 3",
-    "Description" : "Description 3",
-    "Image" : Event3}
-]
+  {
+    Title: "Event 1",
+    Date: "01/01/2020",
+    Time: "12:00",
+    Location: "Location 1",
+    Description: placeholderText,
+    Image: Event1,
+  },
+  {
+    Title: "Event 2",
+    Date: "01/01/2020",
+    Time: "12:00",
+    Location: "Location 2",
+    Description: placeholderText,
+    Image: Event2,
+  },
+  {
+    Title: "Event 3",
+    Date: "01/01/2020",
+    Time: "12:00",
+    Location: "Location 3",
+    Description: placeholderText,
+    Image: Event3,
+  },
+];
 
 function Event(props) {
-    return (
-        <>
-        <div className="event">
-            <img src={props.Image} alt={props.Title} />
+  return (
+    <>
+      <div className="event">
+        <img src={props.Image} alt={props.Title} />
+        <div className="event-info">
+          <p className="event-title">
             {props.Title}
-            {props.Date}
-            {props.Time}
-            {props.Location}
+            <span className="event-quantum">
+              {props.Location}, {props.Date} / {props.Time}
+            </span>
+          </p>
+
+          <span className="event-description">{props.Description}</span>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 function Events() {
-
-    return (
-        <>
-        <div className="events" id="Events">
-            <div className="title"> Events </div>
-            &nbsp;
-            {events.map((item, index)=>{
-                return <Event key={index} Title={item.Title} Date={item.Date} Time={item.Time} Location={item.Location} Description={item.Description} Image={item.Image} />
-            })}
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="events" id="Events">
+        <div className="title"> Upcoming Events </div>
+        {events.map((item, index) => {
+          return (
+            <Event
+              key={index}
+              Title={item.Title}
+              Date={item.Date}
+              Time={item.Time}
+              Location={item.Location}
+              Description={item.Description}
+              Image={item.Image}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Events;
