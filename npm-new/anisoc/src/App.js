bbe96b550a165468f499a305da2717539ebb7e9b
Leo Durrant <poweredbykurisu@hotmail.com>
2022-07-25 19:52:12 +0100

    feat: added polls section

--- npm-new/anisoc/src/App.js
+++ npm-new/anisoc/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/home.js';
-import Menu from './components/menu.js';
-import About from './components/about.js';
-import JoinUs from './components/joinus';
-import Events from './components/events';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home.js";
+import Menu from "./components/menu.js";
+import About from "./components/about.js";
+import JoinUs from "./components/joinus";
+import Events from "./components/events";
+import Polls from "./components/polls";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <About />
         <JoinUs />
         <Events />
+        <Polls />
       </div>
     </>
   );
---
+++ npm-new/anisoc/src/components/polls.js
@@ -0,0 +1,56 @@
+import "../index.css";
+
+const placeholderText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum egestas neque at justo tincidunt egestas. Integer mauris diam, fringilla eget gravida condimentum, lacinia ut justo. Proin cursus dolor a laoreet luctus. Vestibulum vitae feugiat quam, condimentum dictum eros. Morbi eleifend lorem sit amet bibendum dignissim. ";
+
+const polls = [
+  {
+    Title: "Poll 1",
+    Description: placeholderText,
+  },
+  {
+    Title: "Poll 2",
+    Description: placeholderText,
+  },
+  {
+    Title: "Poll 3",
+    Description: placeholderText,
+  },
+];
+
+function Poll(props) {
+  return (
+    <>
+      <div className="poll">
+        <p className="event-title">{props.Title}</p>
+        <p className="event-description">{props.Description}</p>
+        <a href="#" className="event-button">
+          Vote Here
+        </a>
+      </div>
+    </>
+  );
+}
+
+function Polls() {
+  return (
+    <div className="polls" id="Polls">
+      <div className="title"> Polls </div>
+      <div className="polls-container">
+        {polls.map((item, index) => {
+          if (index > 1) return <></>;
+
+          return (
+            <Poll
+              key={index}
+              Title={item.Title}
+              Description={item.Description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Polls;
