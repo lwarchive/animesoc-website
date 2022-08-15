e82bfed4ac1fef09cb39589016567ad354c13f85
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-15 21:48:39 +0100

    wip: added responsiveness to about and events

--- npm-new/anisoc/src/components/about.js
+++ npm-new/anisoc/src/components/about.js
@@ -1,20 +1,19 @@
-import '../index.css';
+import "../index.css";
 
-const aboutCopy = require('../json/about.json');
+const aboutCopy = require("../json/about.json");
 
- //TODO: fetch from api
+//TODO: fetch from api
 
 function About() {
-
-    return (
-        <>
-        <div className="about" id="About">
-            <div className="title"> About </div>
-            &nbsp;
-            <div className="copy"> {aboutCopy.about} </div>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className="about text-center lg:text-left" id="About">
+        <div className="title"> About </div>
+        &nbsp;
+        <div className="copy"> {aboutCopy.about} </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
