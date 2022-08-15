a38e0769219f8ea1354ac2937c3fee46e9fe78c0
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-15 23:22:38 +0100

    wip: polls section now responsive
    TODO: fix button placement

--- npm-new/anisoc/src/components/polls.js
+++ npm-new/anisoc/src/components/polls.js
@@ -5,10 +5,10 @@ const polls = require("../json/polls.json");
 function Poll(props) {
   return (
     <>
-      <div className="poll">
-        <p className="item-title">{props.title}</p>
+      <div className="poll w-full lg:w-1/2 ">
+        <p className="item-title mt-4">{props.title}</p>
         <p className="item-description">{props.description}</p>
-        <button className="item-button">Vote Here</button>
+        <button className="item-button mt-4">Vote Here</button>
       </div>
     </>
   );
@@ -16,9 +16,9 @@ function Poll(props) {
 
 function Polls() {
   return (
-    <div className="polls" id="Polls">
+    <div className="polls bg-cover bg-center" id="Polls">
       <div className="title"> Polls </div>
-      <div className="polls-container">
+      <div className="polls-container flex flex-col w-full lg:flex-row rounded">
         {polls.map((item, index) => {
           if (index > 1) return <></>;
 
