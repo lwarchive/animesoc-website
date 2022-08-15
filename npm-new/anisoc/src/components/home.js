7dbaa2c171ef5b4cd111a7bd72de02470f932ae3
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-15 20:42:22 +0100

    wip: improved responsiveness of the home component

--- npm-new/anisoc/src/components/home.js
+++ npm-new/anisoc/src/components/home.js
@@ -8,14 +8,20 @@ function Home() {
     <>
       <div className="home">
         <div className="home-title-bg">
-          <div className="home-title">
-            <img src="static/logo.png" alt="AniSoc Logo" />
-            <div className="home-title-copy">
-              <div className="subtitle">
+          <div className="home-title flex flex-col text-center lg:flex-row pt-8 pb-8">
+            <img
+              className="w-40 lg:w-2/12"
+              src="static/logo.png"
+              alt="AniSoc Logo"
+            />
+            <div className="my-5 mx-5 lg:w-1/2">
+              <div className="text-lg lg:text-4xl">
                 Royal Holloway, University of London
               </div>
-              <div className="title"> Anime &amp; Manga Society </div>
-              <div className="copy"> {titleCopy.desc} </div>
+              <div className="text-5xl lg:text-6xl">
+                Anime &amp; Manga Society
+              </div>
+              <div className="text-base lg:text-lg"> {titleCopy.desc} </div>
             </div>
           </div>
         </div>
