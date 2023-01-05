43ecd7b23ddb156f6c65aa957dbc6a20345bb14e
Leo Durrant <poweredbykurisu@hotmail.com>
2023-01-05 14:38:32 +0000

    feat: data now fetches correctly

--- components/events.tsx
+++ components/events.tsx
@@ -1,9 +1,6 @@
-import { ParseResult } from "papaparse";
-import { usePapaParse } from "react-papaparse";
+import { parse, ParseResult } from "papaparse";
+import React, { useEffect, useState } from "react";
 
-import events from "./data/events.json";
-
-const { readString } = usePapaParse();
 const dataSource = `https://docs.google.com/spreadsheets/d/e/2PACX-1vRAHUQzFuZ1O0J7soL5Wud5CEbA3MLv4T4Pqms_KhAueNoFX6h2T0DTGwgaLu92FYWdnFV50Q0F1AHY/pub?gid=0&single=true&output=csv`;
 //const dataSource = "";
 
@@ -23,59 +20,6 @@ interface CalendarData {
   startDate: string;
   endDate: string;
   imageURL: string;
-}
-
-interface CSVBuilderProps {
-  [key: string]: string;
-}
-
-// async function fetchEventData(): Promise<ParseResult<CalendarData>> {
-//   return new Promise((resolve, reject) => {
-//     fetch(dataSource)
-//       .then((response) => {
-//         response.text().then((text) => {
-//           readString(text, {
-//             header: true,
-//             worker: true,
-//             complete: (results: ParseResult<CalendarData>) => {
-//               console.log(results.data);
-//               resolve(results);
-//             },
-//           });
-//         });
-//       })
-//       .catch(() => {
-//         reject(new Error(`Cannot retrieve CSV`));
-//       });
-//     return [];
-//   });
-// }
-
-function csvToMap(data: string, delimiter: string) {
-  const headers = data.slice(0, data.indexOf("\n")).split(delimiter);
-  const rows = data.slice(data.indexOf("\n") + 1).split("\n");
-
-  const output = rows.map(function (row) {
-    const values = row.split(delimiter);
-    const elem = headers.reduce(function (object, header, index) {
-      object[header] = values[index];
-      return object;
-    }, {} as CSVBuilderProps);
-    return elem;
-  });
-
-  return output;
-}
-
-async function fetchEventData(): Promise<string> {
-  const result = await fetch(dataSource);
-
-  if (!result.ok) {
-    throw new Error("Failed to fetch event data.");
-  }
-
-  const dataText = await result.text();
-  return dataText;
 }
 
 function Event(props: EventProps) {
@@ -98,31 +42,85 @@ function Event(props: EventProps) {
   );
 }
 
-async function Events() {
-  let eventData = csvToMap(await fetchEventData(), ",");
-  console.log(eventData);
+// export default async function Events() {
+//   let eventData = parse(await fetchEventData());
+//   console.log(eventData);
+//   return (
+//     <>
+//       <div className="events" id="events">
+//         <div className="title text-center md:text-right"> Upcoming Events </div>
+//         {/* {eventData.map((item, index) => {
+//           const dateTime = new Date(item.startDate);
+
+//           return (
+//             <Event
+//               key={index}
+//               title={item.title}
+//               date={dateTime.toLocaleDateString()}
+//               time={dateTime.toLocaleTimeString()}
+//               location={item.location}
+//               description={item.description}
+//               image={item.imageURL}
+//             />
+//           );
+//         })} */}
+//       </div>
+//     </>
+//   );
+// }
+
+function test(data: CalendarData[]) {
+  console.log(data);
+
+  return <span>d</span>;
+}
+
+const Events = () => {
+  const [events, updateEvents] = useState<CalendarData[]>();
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const resp = await fetch(dataSource);
+      const data = await resp.text();
+
+      // TODO: Handle errors
+      const parsedResult = await parse(data, {
+        header: true,
+      });
+      const parsedData = (parsedResult as ParseResult<CalendarData>).data;
+
+      updateEvents(parsedData);
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <>
       <div className="events" id="events">
         <div className="title text-center md:text-right"> Upcoming Events </div>
-        {/* {eventData.map((item, index) => {
-          const dateTime = new Date(item.startDate);
+        {events ? (
+          <>
+            {events.map((item, index) => {
+              const dateTime = new Date(item.startDate);
 
-          return (
-            <Event
-              key={index}
-              title={item.title}
-              date={dateTime.toLocaleDateString()}
-              time={dateTime.toLocaleTimeString()}
-              location={item.location}
-              description={item.description}
-              image={item.imageURL}
-            />
-          );
-        })} */}
+              return (
+                <Event
+                  key={index}
+                  title={item.title}
+                  date={dateTime.toLocaleDateString()}
+                  time={dateTime.toLocaleTimeString()}
+                  location={item.location}
+                  description={item.description}
+                  image={item.imageURL}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <>Loading...</>
+        )}
       </div>
     </>
   );
-}
+};
 
 export default Events;
