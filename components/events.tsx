1658eeed899ca531b81b9d8ac970789d3fe1a537
Leo Durrant <poweredbykurisu@hotmail.com>
2023-01-05 02:19:26 +0000

    pain

--- components/events.tsx
+++ components/events.tsx
@@ -1,8 +1,11 @@
+import { ParseResult } from "papaparse";
 import { usePapaParse } from "react-papaparse";
+
 import events from "./data/events.json";
 
 const { readString } = usePapaParse();
 const dataSource = `https://docs.google.com/spreadsheets/d/e/2PACX-1vRAHUQzFuZ1O0J7soL5Wud5CEbA3MLv4T4Pqms_KhAueNoFX6h2T0DTGwgaLu92FYWdnFV50Q0F1AHY/pub?gid=0&single=true&output=csv`;
+//const dataSource = "";
 
 interface EventProps {
   title: string;
@@ -22,20 +25,57 @@ interface CalendarData {
   imageURL: string;
 }
 
-function fetchEventData(): CalendarData[] {
-  fetch(dataSource).then((response) => {
-    response.text().then((text) => {
-      readString(text, {
-        header: true,
-        worker: true,
-        complete: (results) => {
-          console.log(results);
-          return results;
-        },
-      });
-    });
+interface CSVBuilderProps {
+  [key: string]: string;
+}
+
+// async function fetchEventData(): Promise<ParseResult<CalendarData>> {
+//   return new Promise((resolve, reject) => {
+//     fetch(dataSource)
+//       .then((response) => {
+//         response.text().then((text) => {
+//           readString(text, {
+//             header: true,
+//             worker: true,
+//             complete: (results: ParseResult<CalendarData>) => {
+//               console.log(results.data);
+//               resolve(results);
+//             },
+//           });
+//         });
+//       })
+//       .catch(() => {
+//         reject(new Error(`Cannot retrieve CSV`));
+//       });
+//     return [];
+//   });
+// }
+
+function csvToMap(data: string, delimiter: string) {
+  const headers = data.slice(0, data.indexOf("\n")).split(delimiter);
+  const rows = data.slice(data.indexOf("\n") + 1).split("\n");
+
+  const output = rows.map(function (row) {
+    const values = row.split(delimiter);
+    const elem = headers.reduce(function (object, header, index) {
+      object[header] = values[index];
+      return object;
+    }, {} as CSVBuilderProps);
+    return elem;
   });
-  return [];
+
+  return output;
+}
+
+async function fetchEventData(): Promise<string> {
+  const result = await fetch(dataSource);
+
+  if (!result.ok) {
+    throw new Error("Failed to fetch event data.");
+  }
+
+  const dataText = await result.text();
+  return dataText;
 }
 
 function Event(props: EventProps) {
@@ -58,27 +98,28 @@ function Event(props: EventProps) {
   );
 }
 
-function Events() {
-  let eventData = fetchEventData();
+async function Events() {
+  let eventData = csvToMap(await fetchEventData(), ",");
+  console.log(eventData);
   return (
     <>
       <div className="events" id="events">
         <div className="title text-center md:text-right"> Upcoming Events </div>
-        {events.eventList.map((item, index) => {
-          const dateTime = item.date.split(" ");
+        {/* {eventData.map((item, index) => {
+          const dateTime = new Date(item.startDate);
 
           return (
             <Event
               key={index}
               title={item.title}
-              date={dateTime[0]}
-              time={dateTime[1]}
+              date={dateTime.toLocaleDateString()}
+              time={dateTime.toLocaleTimeString()}
               location={item.location}
               description={item.description}
-              image={item.image}
+              image={item.imageURL}
             />
           );
-        })}
+        })} */}
       </div>
     </>
   );
