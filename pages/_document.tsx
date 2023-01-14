e3ff146011d12ea22d3e1e00d4aa881c39852ce0
Leo Durrant <poweredbykurisu@hotmail.com>
2023-01-14 17:43:44 +0000

    chore: cleaned up root document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,10 @@ function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600;700&amp;display=swap"
@@ -24,7 +19,7 @@ function Document() {
         />
         <meta
           name="description"
-          content="CS Student, programmer, and keen Dr.Pepper drinker"
+          content="Welcome to the RHUL Anime and Manga Society website"
         />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1" />
         <meta
