37ae6d21cf40b5d1baed315f24cd13a640a1a4d5
Leo Durrant <poweredbykurisu@hotmail.com>
2024-04-03 22:47:31 +0100

    fix: changed calendar used

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,8 +24,8 @@ const Landing: NextPage = () => {
       <About />
       <JoinUs />
       <Events
-        dataSource={`https://docs.google.com/spreadsheets/d/e/2PACX-1vRAHUQzFuZ1O0J7soL5Wud5CEbA3MLv4T4Pqms_KhAueNoFX6h2T0DTGwgaLu92FYWdnFV50Q0F1AHY/pub?gid=0&single=true&output=csv`}
-        calendar={`https://calendar.google.com/calendar/embed?src=c_8219055ac4671ef4f7faec2be6f2db0d38d1e3b23c3b4e94d81b71fdc3c6a0e4%40group.calendar.google.com&ctz=Europe%2FLondon`}
+        dataSource={`https://docs.google.com/spreadsheets/d/14dy-jvnqQi-1PsGs5t3sKUsHsZYAw-yW-lsV_uFEWdA/pub?gid=0&single=true&output=csv`}
+        calendar={`https://calendar.google.com/calendar/embed?src=6398b1b51dbc3cb981175df6bc3f75a88040fc0e9b006dbdc6d160e26b4d40db%40group.calendar.google.com&ctz=Europe%2FLondon`}
       />
       <Committee />
       <Footer />
