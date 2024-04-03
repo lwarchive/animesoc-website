e42ee41b406fbc1596a58994fc473bf925708fb2
Leo Durrant <poweredbykurisu@hotmail.com>
2024-04-04 00:47:58 +0100

    fix: meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -65,22 +65,23 @@ function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/rhulanisoc/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/rhulanisoc/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/rhulanisoc/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f59e45" />
+        <link rel="icon" type="image/x-icon" href="/rhulanisoc/favicon.ico"></link>
+        <link rel="manifest" href="/rhulanisoc/site.webmanifest" />
+        <link rel="mask-icon" href="/rhulanisoc/safari-pinned-tab.svg" color="#f59e45" />
         <meta name="msapplication-TileColor" content="#f59e45" />
         <meta name="theme-color" content="#f59e45" />
       </Head>
